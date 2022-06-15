b9c6eecef0b7235e408b0b0d4a10bce9eae13db2
Haxrox <56497846+Haxrox@users.noreply.github.com>
2022-06-14 21:39:05 -0700

    fixed import

--- ProjectsDetailedGet/index.js
+++ ProjectsDetailedGet/index.js
@@ -1,4 +1,4 @@
-const ProjectsService = require("../services/projectsService.js");
+const ProjectsService = require("../Services/projectsService.js");
 
 module.exports = async function (context, req) {
     await ProjectsService.getDetailedProject(context);
